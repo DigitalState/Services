6e9b9e87a2cc7d2a1c574e333a5e6d322b9eb12f
mario <mario@digitalstate.ca>
2017-05-27 12:41:16 -0400

    Add bpm services

--- src/Ds/Bundle/BpmBundle/DependencyInjection/DsBpmExtension.php
+++ src/Ds/Bundle/BpmBundle/DependencyInjection/DsBpmExtension.php
@@ -21,5 +21,6 @@ class DsBpmExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
