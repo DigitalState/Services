3a6166e9edb6f9db8f62afb983ffef611c256ad8
mario <mario@digitalstate.ca>
2017-06-03 12:16:49 -0400

    Add submission formio dryrun listener

--- src/Ds/Bundle/ServiceBundle/EventListener/Submission/FormioListener.php
+++ src/Ds/Bundle/ServiceBundle/EventListener/Submission/FormioListener.php
@@ -5,6 +5,9 @@ namespace Ds\Bundle\ServiceBundle\EventListener\Submission;
 use Ds\Component\Formio\Api\Api;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Ds\Bundle\ServiceBundle\Entity\Submission;
+use Ds\Component\Formio\Model\Submission as Model;
+use Ds\Component\Formio\Query\SubmissionParameters as Parameters;
+
 
 /**
  * Class FormioListener
@@ -39,6 +42,13 @@ class FormioListener
             return;
         }
 
-        $submission = $entity;
+//        $submission = $entity;
+//        $model = new Model;
+//        $model
+//            ->setData((object) $submission->getData());
+//        $parameters = new Parameters;
+//        $parameters
+//            ->setDryRun(true);
+//        $this->api->submission->create($model, $parameters);
     }
 }
