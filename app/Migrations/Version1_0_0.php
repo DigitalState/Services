b9ee1eb31eea2090a7677ca12f8e7a85c90322dc
mario <mario@digitalstate.ca>
2017-06-17 13:07:46 -0400

    Set migration versions to use semantic versioning instead of timestamp

--- app/Migrations/Version1_0_0.php
+++ app/Migrations/Version1_0_0.php
@@ -6,9 +6,9 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Class Version20170616030220
+ * Class Version1_0_0
  */
-class Version20170616030220 extends AbstractMigration
+class Version1_0_0 extends AbstractMigration
 {
     /**
      * Up
