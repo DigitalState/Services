a83da468da402b2258d5f5e421bc3e18b53afd2d
mario <mario@digitalstate.ca>
2017-09-10 15:11:51 -0400

    Restructure scenario data validator to individual method per types

--- src/AppBundle/Validator/Constraints/Scenario/Data/ValidValidator.php
+++ src/AppBundle/Validator/Constraints/Scenario/Data/ValidValidator.php
@@ -19,24 +19,73 @@ class ValidValidator extends ConstraintValidator
         $data = $scenario->getData();
 
         switch ($scenario->getType()) {
-            case Scenario::TYPE_BPM:
-                foreach (['bpm', 'process_definition_key'] as $attribute) {
-                    if (!array_key_exists($attribute, $data)) {
-                        $this->context
-                            ->buildViolation($constraint->missing)
-                            ->setParameter('{{ attribute }}', '"'.$attribute.'""')
-                            ->atPath('data.'.$attribute)
-                            ->addViolation();
-                    }
-                }
-
+            case Scenario::TYPE_API:
+                $this->validateApi($data, $constraint);
                 break;
 
-            case Scenario::TYPE_API:
-            case Scenario::TYPE_INFO:
-            case Scenario::TYPE_URL:
+            case Scenario::TYPE_BPM:
+                $this->validateBpm($data, $constraint);
+                break;
 
+            case Scenario::TYPE_INFO:
+                $this->validateInfo($data, $constraint);
+                break;
+
+            case Scenario::TYPE_URL:
+                $this->validateUrl($data, $constraint);
                 break;
         }
+    }
+
+    /**
+     * Validate api scenario data
+     *
+     * @param array $data
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     */
+    protected function validateApi(array $data, Constraint $constraint)
+    {
+
+    }
+
+    /**
+     * Validate bpm scenario data
+     *
+     * @param array $data
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     */
+    protected function validateBpm(array $data, Constraint $constraint)
+    {
+        foreach (['bpm', 'process_definition_key'] as $attribute) {
+            if (!array_key_exists($attribute, $data)) {
+                $this->context
+                    ->buildViolation($constraint->missing)
+                    ->setParameter('{{ attribute }}', '"'.$attribute.'"')
+                    ->atPath('data.'.$attribute)
+                    ->addViolation();
+            }
+        }
+    }
+
+    /**
+     * Validate info scenario data
+     *
+     * @param array $data
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     */
+    protected function validateInfo(array $data, Constraint $constraint)
+    {
+
+    }
+
+    /**
+     * Validate url scenario data
+     *
+     * @param array $data
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     */
+    protected function validateUrl(array $data, Constraint $constraint)
+    {
+
     }
 }
