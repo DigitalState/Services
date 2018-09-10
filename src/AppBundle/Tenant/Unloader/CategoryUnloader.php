22958ee7f147c0bdc8ec439aa6bec6507f6c1055
Mario Prud'homme <mario@prudhom.me>
2018-09-10 13:48:22 -0400

    Merge feature/tenant purge (#53)

---
+++ src/AppBundle/Tenant/Unloader/CategoryUnloader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Tenant\Unloader;
+
+use AppBundle\Entity\Category;
+use Doctrine\ORM\EntityManager;
+use Ds\Component\Tenant\Entity\Tenant;
+use Ds\Component\Tenant\Loader\Unloader;
+
+/**
+ * Class CategoryUnloader
+ */
+class CategoryUnloader implements Unloader
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\ORM\EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unload(Tenant $tenant)
+    {
+        $builder = $this->entityManager->getRepository(Category::class)->createQueryBuilder('e');
+        $builder
+            ->delete()
+            ->where('e.tenant = :tenant')
+            ->setParameter('tenant', $tenant->getUuid());
+        $query = $builder->getQuery();
+        $query->execute();
+    }
+}
---
+++ src/AppBundle/Tenant/Unloader/SubmissionUnloader.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Tenant\Unloader;
+
+use AppBundle\Entity\Submission;
+use Doctrine\ORM\EntityManager;
+use Ds\Component\Tenant\Entity\Tenant;
+use Ds\Component\Tenant\Loader\Unloader;
+
+/**
+ * Class SubmissionUnloader
+ */
+class SubmissionUnloader implements Unloader
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\ORM\EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unload(Tenant $tenant)
+    {
+        $builder = $this->entityManager->getRepository(Submission::class)->createQueryBuilder('e');
+        $builder
+            ->delete()
+            ->where('e.tenant = :tenant')
+            ->setParameter('tenant', $tenant->getUuid());
+        $query = $builder->getQuery();
+        $query->execute();
+    }
+}
