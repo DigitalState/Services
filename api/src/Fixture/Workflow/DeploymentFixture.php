1eca0d5c2e580c5fa480a57c0c10e8a4f7bc34aa
mario-digitalstate <mario@digitalstate.ca>
2019-01-03 11:33:46 -0500

    Debug travis

--- api/src/Fixture/Workflow/DeploymentFixture.php
+++ api/src/Fixture/Workflow/DeploymentFixture.php
@@ -4,15 +4,21 @@ namespace App\Fixture\Workflow;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 use Ds\Component\Camunda\Fixture\Camunda\Deployment;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Ds\Component\Api\Api\Api;
+use Ds\Component\Camunda\Model\Deployment as DeploymentModel;
+use Ds\Component\Camunda\Query\DeploymentParameters;
+use Ds\Component\Database\Fixture\Yaml;
 
 /**
  * Class DeploymentFixture
  */
 final class DeploymentFixture implements FixtureInterface, OrderedFixtureInterface, ContainerAwareInterface
 {
-    use Deployment;
+    use Yaml;
+//    use Deployment;
 
     /**
      * Constructor
@@ -33,5 +39,57 @@ final class DeploymentFixture implements FixtureInterface, OrderedFixtureInterfa
     public function getOrder()
     {
         return 30;
+    }
+
+    /**
+     * @var string
+     */
+    private $app;
+
+    /**
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $fixtures = array_key_exists('FIXTURES', $_ENV) ? $_ENV['FIXTURES'] : 'dev';
+        $source = $this->namespace.'.'.$this->app.'.fixtures.'.$fixtures;
+        $api = $this->container->get(Api::class)->get('workflow.deployment');
+        $parameters = new DeploymentParameters;
+        $parameters->setSource($source);
+        $deployments = $api->getList($parameters);
+
+        foreach ($deployments as $deployment) {
+            $parameters = new DeploymentParameters;
+            $parameters->setCascade(true);
+            $api->delete($deployment->getId(), $parameters);
+        }
+
+        $objects = $this->parse($this->path);
+
+        foreach ($objects as $object) {
+            $deployment = new DeploymentModel;
+            $deployment
+                ->setName($object->name)
+                ->setSource($source)
+                ->setTenantId($object->tenant_id);
+            $files = [];
+
+            foreach ($object->files as $file) {
+                $files[] = dirname(str_replace('{fixtures}', $fixtures, $this->path)).'/'.$file;
+            }
+
+            $deployment->setFiles($files);
+            $api->create($deployment);
+        }
     }
 }
